9f0b498e4c1ffecbe029f9b8ea71a1a8b35f2f74
aries <aries@192.168.0.171>
2024-10-14 23:10:55 -0600

    error corregido

---
+++ src/main/java/com/mycompany/quedeahuevooficial/QueDeAhuevoOficial.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.quedeahuevooficial;
+
+/**
+ *
+ * @author aries
+ */
+public class QueDeAhuevoOficial {
+
+    public static void main(String[] args) {
+        var principal = new Principal();
+        principal.setVisible(true);
+    }
+}

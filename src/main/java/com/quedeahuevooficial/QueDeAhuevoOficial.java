40c14c3adb14c14f38c4aa2ae5d5cf8f97d7735c
aries <aries@192.168.0.171>
2024-10-14 22:37:33 -0600

    Comienzo

---
+++ src/main/java/com/quedeahuevooficial/QueDeAhuevoOficial.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.quedeahuevooficial;
+
+/**
+ *
+ * @author aries
+ */
+public class QueDeAhuevoOficial {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
